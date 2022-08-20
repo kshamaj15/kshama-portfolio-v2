d7508b3284b6c5ed63054e47d31b4791f036cf6b
kshamaj15 <kshamaj150896@gmail.com>
2022-08-20 22:38:52 +0530

    deployment script

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.css';
+import TopBar from './components/TopBar';
+import { THEME } from './constant';
 
-function App() {
+const App = () => {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? <div id="loading"></div> :
+        <div className="container">
+          <TopBar />
+          <div>
+          </div>
+          <div>
+
+          </div>
+        </div>}
+    </>
   );
 }
 
