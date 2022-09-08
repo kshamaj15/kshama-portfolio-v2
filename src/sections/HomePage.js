56002a9d3a8422c8c8c2e09a425eb41b7530ae3e
kshamaj15 <kshamaj150896@gmail.com>
2022-09-08 10:53:18 +0530

    resume url updated

--- src/sections/HomePage.js
+++ src/sections/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = () => {
             <h1 className='my-name'>Kshama Jain</h1>
             <p className='my-details'>I'm a Software Engineer with more than 4 years of experience in designing and developing large scale web applications with React, JavaScript, TypeScipt, Node JS. </p>
             <ButtonWrapper>
-                <a rel="noreferrer" href='https://drive.google.com/file/d/1n8NwPn7FdT2PbTXMHZbBdVaIaOT8q-ns/view?usp=sharing' target='_blank'>My Resume</a>
+                <a rel="noreferrer" href='https://drive.google.com/file/d/1n8NwPn7FdT2PbTXMHZbBdVaIaOT8q-ns/view' target='_blank'>My Resume</a>
             </ButtonWrapper>
         </section>
     )
